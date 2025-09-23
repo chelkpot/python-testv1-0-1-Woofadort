88f46a9a2441770d0d35195f0e18fbe2622f9e14
Woofadort <162667272+Woofadort@users.noreply.github.com>
2025-09-23 23:42:57 +0500

    Решение задач

--- tasks/task3.py
+++ tasks/task3.py
@@ -4,7 +4,16 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
+    weight: str = input("Введите вес в кг: ").strip().replace(',', '.')
+    height: str = input("Введите рост в метрах: ").strip().replace(',', '.')
 
+    weight = float(weight)
+    height = float(height)
+
+    BMI = weight / (height**2)
+
+    print(f"Индекс массы тела: {BMI}")
+    # Аннотация типов - не признак ИИ
    
 
 # Код ниже не трогать! он нужен для тестов
